df9f6dcce40b0c247ac2ac1bc655aa5d4d360172
blazikenEX <alvee6176@gmail.com>
2025-08-09 17:48:03 +0300

    responses edit

--- responses.py
+++ responses.py
@@ -1,17 +1,13 @@
 import random
 responses = [
     "Hello! How can I help you?",
-    "What's on your mind?",
-    "I'm here to assist you. What do you need?",
-    "How can I assist you?",
-    "What can I help you with?",
 ]
 
 def chatbot_responses(user_input):
   user_input = user_input.lower()
 
-  if "movie" in user_input:
-    return "I recommend checking out the IMDb website for movie recommendations. They have a wide variety of genres and ratings to choose from."
+  if "campus map" in user_input:
+    return "I recommend checking out the aalto usefuk map for movie recommendations. They have a wide variety of genres and ratings to choose from."
   elif "weather" in user_input:
     return "You can check the weather by using a weather website or app. Some popular ones include Weather.com and The Weather Channel app."
   elif "news" in user_input:
