d195dd1a35e134d3e295f9246b6d8598c2589267
blazikenEX <alvee6176@gmail.com>
2025-08-09 18:38:13 +0300

    fixed responses logic

--- responses.py
+++ responses.py
@@ -1,4 +1,3 @@
-import random
 responses = [
     "Hello! How can I help you?",
 ]
@@ -6,14 +5,14 @@ responses = [
 def chatbot_responses(user_input):
   user_input = user_input.lower()
 
-  if "map" or "direction" in user_input:
+  if "map" in user_input or "direction" in user_input:
     return "You can find useful directions about the Aalto campus from this map - usefulaaltomap.fi  . "
-  elif "food" or "lunch" or "meal" in user_input:
+  elif "food" in user_input or "lunch" in user_input or "meal" in user_input:
     return "You can check this website for live menu for all the student restaurant at aalto - kanttiinit.fi   ."
-  elif "marketplace" or "buy" in user_input:
+  elif "marketplace" in user_input or "buy" in user_input:
     return "There is a second-hand buy and sell group for aalto students"
   elif "joke" in user_input:
     return "Why couldn't the bicycle stand up by itself? Because it was two-tired!"
   else:
-    return random.choice(responses)
+    return ("sorry don't know")
 
